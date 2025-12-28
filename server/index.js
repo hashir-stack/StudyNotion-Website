32ff6e6a3486bbfe29c63de401e6b5f9fc90d162
hashir-stack <hashir7ansari@gmail.com>
2025-12-28 14:43:41 +0530

    errors

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ app.use("/api/v1/course",courseRoutes);
 
 // activate the server
 if(process.env.NODE_ENV !== "production"){
-  server.listen(port, () => {
+  app.listen(port, () => {
   console.log(`Server is successfully running on ${port}...`);
   });
 }
