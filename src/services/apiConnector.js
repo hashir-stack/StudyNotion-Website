62551e5f1dd3a78f19b5b06237b1242278bc61b0
hashir-stack <hashir7ansari@gmail.com>
2025-12-29 15:23:24 +0530

    fixing the errors

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -1,43 +1,30 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//     withCredentials: true
-// });
-
-// export const apiConnector = (method, url, bodyData, headers, params)=>{
-//     return axiosInstance({
-//         method,
-//         url,
-//         data: bodyData ? bodyData : null,
-//         headers: headers ? headers : null,
-//         params: params ? params : null,
-//     });
-// };
-
 import axios from "axios";
 
 export const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-// Add interceptor to attach token
+// Request interceptor to attach token
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${token}`,
+      };
     }
     return config;
   },
   (error) => Promise.reject(error)
 );
 
-export const apiConnector = (method, url, bodyData, headers, params) => {
+export const apiConnector = (method, url, bodyData, headers = {}, params = {}) => {
   return axiosInstance({
     method,
     url,
     data: bodyData || null,
-    headers: headers || {},
-    params: params || null,
+    headers: { ...headers },   // ✅ only custom headers
+    params: { ...params },
   });
 };
